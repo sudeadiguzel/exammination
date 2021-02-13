613a019f14d6c472687a956a9e03fa3b36d2e79b
Dogukan.Kutuk <dogukan.kutuk@kron.com.tr>
2021-02-13 18:20:25 +0300

    exam repository paging query examples added.

--- src/main/java/com/cloud/examsystem/exam/repository/ExamRepository.java
+++ src/main/java/com/cloud/examsystem/exam/repository/ExamRepository.java
@@ -1,7 +1,10 @@
 package com.cloud.examsystem.exam.repository;
 
 import com.cloud.examsystem.exam.entity.Exam;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -9,4 +12,11 @@ import java.util.Optional;
 @Repository
 public interface ExamRepository extends JpaRepository<Exam,Long> {
     Optional<Exam> getById(Long id);
+
+    @Query("FROM Exam e")
+    Page<Exam> findActiveRecords(Pageable p);
+
+    @Query("FROM Exam e where e.user.db_id=:id and e.user.instructor=true")
+    Page<Exam> findActiveRecordsForInstructor(Long id,Pageable p);
+
 }
