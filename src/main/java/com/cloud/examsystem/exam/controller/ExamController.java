745ac073e9fec6beb6add5cc3c2f955b9ceb1fe3
Dogukan.Kutuk <dogukan.kutuk@kron.com.tr>
2021-02-13 18:19:10 +0300

    exam student controller updated

--- src/main/java/com/cloud/examsystem/exam/controller/ExamController.java
+++ src/main/java/com/cloud/examsystem/exam/controller/ExamController.java
@@ -1,15 +1,16 @@
 package com.cloud.examsystem.exam.controller;
 
 
+import com.cloud.examsystem.common.dto.DatatableRequest;
+import com.cloud.examsystem.common.util.PaginationUtils;
 import com.cloud.examsystem.exam.entity.Exam;
 import com.cloud.examsystem.exam.service.ExamService;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.Map;
 import java.util.Optional;
 
 @Controller
@@ -19,27 +20,33 @@ public class ExamController {
     private final ExamService examService;
 
     @GetMapping("/{id}") //exam getter
-    public String getExamById(@RequestParam("id") Long id, Model model){
+    public String getExamById(@PathVariable("id") Long id, Model model) {
         Optional<Exam> examOpt = examService.getExamById(id);
-        if (!examOpt.isPresent()){
+        if (!examOpt.isPresent()) {
             return "/error";
         }
         model.addAttribute("data", examOpt.get());
-        return "/exam/apply/".concat(id.toString());
+        return "/exam/apply";
     }
-//
-//    @GetMapping("/create")
-//    public String createQuestionPage(Model model){
-//        model.addAttribute("question_number",3);
-//        model.addAttribute("option_number",4);
-//        return "exam/create_exam";
-//    }
-    //@GetMapping("/create")
-    //public String createExamPage(Model model){
-    //    model.addAttribute("question_number",3);
-   //     return "/exam/create_exam";
-   // }
 
+    @GetMapping("/list")
+    public String getExamListPage(Model model) {
+        return "/exam/exam_list";
+    }
+
+    @PostMapping("/list")
+    @ResponseBody
+    public Map getListWithPagination(@ModelAttribute DatatableRequest request) {
+        return PaginationUtils.createResultSet(examService.getAll(request), request);
+    }
+
+
+    @GetMapping("/create")
+    public String createQuestionPage(Model model){
+        model.addAttribute("question_number",3);
+        model.addAttribute("option_number",4);
+        return "exam/create_exam";
+    }
 
 
 
