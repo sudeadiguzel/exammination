f9c0d8537972d892e4c27f5f12118c6ff1083a4d
Dogukan.Kutuk <dogukan.kutuk@kron.com.tr>
2021-02-13 20:45:51 +0300

    exam list for instructor service has been added.

--- src/main/java/com/cloud/examsystem/exam/service/ExamService.java
+++ src/main/java/com/cloud/examsystem/exam/service/ExamService.java
@@ -3,6 +3,8 @@ package com.cloud.examsystem.exam.service;
 import com.cloud.examsystem.common.dto.DatatableRequest;
 import com.cloud.examsystem.exam.entity.Exam;
 import com.cloud.examsystem.exam.repository.ExamRepository;
+import com.cloud.examsystem.user.entity.User;
+import com.cloud.examsystem.user.service.UserAuthService;
 import lombok.AllArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Service;
@@ -13,6 +15,7 @@ import java.util.Optional;
 @AllArgsConstructor
 public class ExamService {
     private final ExamRepository examRepository;
+    private final UserAuthService userAuthService;
 
     public Optional<Exam> getExamById(Long id) {
         return examRepository.getById(id);
@@ -26,7 +29,8 @@ public class ExamService {
         return examRepository.findActiveRecords(request.toPageRequest());
     }
 
-    public Page<Exam> getAllForInstructor(Long instructorId, DatatableRequest request) {
-        return examRepository.findActiveRecordsForInstructor(instructorId, request.toPageRequest());
+    public Page<Exam> getAllForInstructor(DatatableRequest request) {
+        User user = userAuthService.getCurrentUser();
+        return examRepository.findActiveRecordsForInstructor(user.getDb_id(), request.toPageRequest());
     }
 }
