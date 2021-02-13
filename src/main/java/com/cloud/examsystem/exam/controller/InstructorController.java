01fb81728c06989506070d573d5296a113c3a1a0
Dogukan.Kutuk <dogukan.kutuk@kron.com.tr>
2021-02-13 20:52:23 +0300

    exam_apply, exam_edit, exam_list.html pages has been added.

--- src/main/java/com/cloud/examsystem/exam/controller/InstructorController.java
+++ src/main/java/com/cloud/examsystem/exam/controller/InstructorController.java
@@ -4,7 +4,6 @@ package com.cloud.examsystem.exam.controller;
 import com.cloud.examsystem.common.dto.DatatableRequest;
 import com.cloud.examsystem.common.util.PaginationUtils;
 import com.cloud.examsystem.exam.entity.Exam;
-import com.cloud.examsystem.exam.model.Option;
 import com.cloud.examsystem.exam.model.Question;
 import com.cloud.examsystem.exam.model.countPage;
 import com.cloud.examsystem.exam.service.ExamService;
@@ -14,7 +13,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Map;
 import java.util.Optional;
+
+
 @Log4j2
 @Controller
 @RequestMapping("/instructor")
@@ -62,21 +64,21 @@ public class InstructorController {
     @PostMapping("a")
     public String postDeneme(Model model)
     {
-        Exam exam=new Exam();
-        for(int i=0;i<this.countPage.getQuestionCount();i++){
-            Question question=new Question();
-            for(int j=0;j<this.countPage.getOptionCount();j++){
-                String optionLabel= (String) model.getAttribute("option-".concat(String.valueOf(j)));
-                Boolean optionAnswerCheck= (Boolean) model.getAttribute("optionAnswerCheck-".concat(String.valueOf(j)));
-                Option option=new Option();
-                option.setContent(optionLabel);
-                option.setCorrect(optionAnswerCheck);
-                question.getOptionList().add(option);
-            }
-            exam.getQuestion().add(question);
-        }
+//        Exam exam=new Exam();
+//        for(int i=0;i<this.countPage.getQuestionCount();i++){
+//            Question question=new Question();
+//            for(int j=0;j<this.countPage.getOptionCount();j++){
+//                String optionLabel= (String) model.getAttribute("option-".concat(String.valueOf(j)));
+//                Boolean optionAnswerCheck= (Boolean) model.getAttribute("optionAnswerCheck-".concat(String.valueOf(j)));
+//                Option option=new Option();
+//                option.setContent(optionLabel);
+//                option.setCorrect(optionAnswerCheck);
+//                question.getOptionList().add(option);
+//            }
+//            exam.getQuestion().add(question);
+//        }
 
-        log.info(exam);
+//        log.info(exam);
         return "/exam/create_exam";
     }
 
